5a05553295a7af674098ae96de809a3332e1e89c
TheAssassin <theassassin@assassinate-you.net>
2018-05-31 20:37:48 +0200

    Travis build, mk. 1

---
+++ travis/travis-build.sh
@@ -0,0 +1,39 @@
+#! /bin/bash
+
+# Copyright 2018 TheAssassin
+# Licensed under the terms of the MIT license.
+
+set -x
+set -e
+
+# use RAM disk if possible
+if [ -d /dev/shm ]; then
+    TEMP_BASE=/dev/shm
+else
+    TEMP_BASE=/tmp
+fi
+
+BUILD_DIR=$(mktemp -d -p "$TEMP_BASE" AppImageUpdate-build-XXXXXX)
+
+cleanup () {
+    if [ -d "$BUILD_DIR" ]; then
+        rm -rf "$BUILD_DIR"
+    fi
+}
+
+trap cleanup EXIT
+
+# store repo root as variable
+REPO_ROOT=$(readlink -f $(dirname $(dirname $0)))
+OLD_CWD=$(readlink -f .)
+
+pushd "$BUILD_DIR"
+
+cmake "$REPO_ROOT"
+
+make -j$(nproc)
+
+# move AppImages to old cwd
+mv *gs-plugin-appimage*.so* "$OLD_CWD"/
+
+popd
